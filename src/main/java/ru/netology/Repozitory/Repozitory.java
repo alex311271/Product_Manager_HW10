6f4fbc2c2aaabe646bb9a4fe6a086267c38f40f8
Aleksey Beketov <alex311271@gmail.com>
2022-10-25 12:02:53 +0300

    init branch homework12

--- src/main/java/ru/netology/Repozitory/Repozitory.java
+++ src/main/java/ru/netology/Repozitory/Repozitory.java
@@ -1,15 +1,16 @@
 package ru.netology.Repozitory;
 
-import ru.netology.Domain.Book;
+import ru.netology.Repozitory.NotFoundException;
 import ru.netology.Domain.Product;
-import ru.netology.Domain.Smartphone;
 
 public class Repozitory {
 
     private Product[] items = new Product[0];
 
-
     public void save(Product item) {
+        if (findId(item.getId()) != null){
+            throw new AlreadyExistsException("ID" + item.getId() + "уже используется");
+        }
         Product[] temp = new Product[items.length + 1];
         for (int i = 0; i < items.length; i++) {
             temp[i] = items[i];
@@ -22,6 +23,11 @@ public class Repozitory {
 
 
     public void removeId(int id) {
+        if (findId(id) == null) {
+           throw new NotFoundException(
+                    "Товар с Id" + id + "не найден"
+            );
+        }
         Product[] temp = new Product[items.length - 1];
         int copyIndex = 0;
         for (Product item : items) {
@@ -35,6 +41,16 @@ public class Repozitory {
 
     public Product[] findAll() {
         return items;
+    }
+
+    public Product[] findId(int id) {
+        for (Product product : items) {
+            if (product.getId() == id) {
+                return items;
+            }
+
+        }
+        return null;
     }
 
 }
